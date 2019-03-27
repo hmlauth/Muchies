a5d353cb1548d8d81ee1b75664e78fc6fb3d7c55
Andromeda <hannalauth@gmail.com>
2019-03-26 22:18:43 -0700

    update URL

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var mysql = require("mysql");
 var connection;
 // define environment
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB); 
+  connection = mysql.createConnection(process.env.JAWSDB_URL); 
 } else {
   connection = mysql.createConnection({
     host: "localhost",
